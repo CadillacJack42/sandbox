54d16b00a0b3065a01d60f8b896fec6304ddecd7
armaBurton <armanarma@armaburton.com>
2021-11-16 13:21:36 -0800

    adding to delete later

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import { gmMonsterArray } from "./gmMonsters.js"
+import { players } from "./playerCharacter.js"
 
 const monsterArray = document.getElementById('gm-monster')
 console.log(monsterArray.item([0]));
 
 const getMonsters = document.getElementById('gm-roll')
 console.log(getMonsters);
+
+console.log(players);
 
 // getMonsters.addEventListener('click', {
 //     for (let i = 0; i < array.length; i++) {
