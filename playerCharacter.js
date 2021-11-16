2a2819882176591f4e8e9c21b7cd440bc3c9cb98
armaBurton <armanarma@armaburton.com>
2021-11-16 13:11:16 -0800

    added player character object

---
+++ playerCharacter.js
@@ -0,0 +1,32 @@
+export const players = [
+    {
+        name: 'archer',
+        attack: Math.floor(Math.random() * 15) + 1,
+        hp: 35,
+        img_url: "https://images6.fanpop.com/image/photos/37300000/Elf-Archer-fantasy-37301920-500-480.jpg"
+    },
+    {
+        name: 'barbarian',
+        attack: Math.floor(Math.random() * 8) + 1,
+        hp: 70,
+        img_url: "https://cdna.artstation.com/p/assets/images/images/002/610/086/large/sergey-grechanyuk-smaller-corrections.jpg?1463604492"
+    },
+    {
+        name: 'warlock',
+        attack: Math.floor(Math.random() * 15) + 1,
+        hp: 25,
+        img_url: "https://i.pinimg.com/originals/b8/e1/f8/b8e1f8cd781de13cbfc4dda8d7127c7f.jpg"
+    },
+    {
+        name: 'cleric',
+        attack: Math.floor(Math.random() * 15) + 1,
+        hp: 30,
+        img_url: "https://i.pinimg.com/originals/49/41/2c/49412c23c19966806e3bd075af48b417.jpg"
+    },
+    {
+        name: 'valkyrie',
+        attack: Math.floor(Math.random() * 8) + 1,
+        hp: 60,
+        img_url: "https://i.pinimg.com/originals/ba/e8/eb/bae8ebbb08a347429cd0047eaf9426ed.jpg"
+    }
+]
